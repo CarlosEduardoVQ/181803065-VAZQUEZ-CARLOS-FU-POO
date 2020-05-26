3c735033dc8eb7050b86be7c148704d93939e4ac
Carlos Eduardo Vazquez Quitl <carlos.vazquez1165@uppuebla.edu.mx>
2020-05-26 00:29:52 -0500

    Estaciones

---
+++ EdadesP/src/main/java/EdadesP.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class EdadesP {
+    
+    public static void main(String[] args) {
+        Scanner dato = new Scanner(System.in);
+        
+        int nalum, edadesa;
+        
+    
+        
+        
+        System.out.print("El número de alumnos es: ");
+        nalum = dato.nextInt();
+        System.out.println("El número de ");
+        edadesa = dato.nextInt();
+        
+       
+        
+    }
+    
+}
