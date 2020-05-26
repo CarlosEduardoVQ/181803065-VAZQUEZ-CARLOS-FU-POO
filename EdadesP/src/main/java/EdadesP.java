deb61ba922b0114db69e71b739fdcdddcb113806
Carlos Eduardo Vazquez Quitl <carlos.vazquez1165@uppuebla.edu.mx>
2020-05-26 15:46:39 -0500

    Juego

--- EdadesP/src/main/java/EdadesP.java
+++ EdadesP/src/main/java/EdadesP.java
@@ -12,11 +12,11 @@ public class EdadesP {
         n_alum = dato.nextInt();
 
         for (int a = 0; a < n_alum; a++) {
-            
-             System.out.println("Ingresa la edad: ");
-             edad = dato.nextInt();
-             suma +=edad;
-             
+
+            System.out.println("Ingresa la edad: ");
+            edad = dato.nextInt();
+            suma += edad;
+
         }
         pro = suma / n_alum;
         System.out.println("El promedio de la edad es: " + pro);
